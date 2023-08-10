812727e7934e35e5942fd22eea1a578ec568401d
Frank <83507659+Frank17@users.noreply.github.com>
2023-08-10 00:49:24 -0700

    Update app.py
    Updated layout and added prompt engineering support for neurodiversity

--- app.py
+++ app.py
@@ -4,21 +4,36 @@ import openai
 import os
 from typing import Iterator
 
+
+ND_PROMPT = ('The following text is typed by an neurodiverse individual. '
+             'Please reply to them in a friendly and appropriate manner. ')
+
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
 st.title('NeuroGPT')
 
+
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+init_prompt = st.selectbox(
+    'You might want to try these prompts...',
+    ['How to socialize?',
+     'How to focus on tasks?',
+     'How to find peace in daily work?']
+)
+chat_room = st.container()
+
+ND_PROMPT
+
 
 if 'history' not in st.session_state:
     st.session_state['history'] = []
 
 for msg in st.session_state['history']:
-    st.chat_message(msg['role']).write(msg['content'])
+    chat_room.chat_message(msg['role']).write(msg['content'])
 
-
-def add_to_history(role: str, content: str) -> None:
+def add_to_history(content: str, role: str) -> None:
     st.session_state['history'].append(
         {'role': role, 'content': content}
     )
@@ -33,22 +48,36 @@ def get_gpt_response(prompt: str) -> Iterator[dict]:
     )
 
 
-placeholder = 'Hi, there! Enter what you want to let me know here.'
-if prompt := st.chat_input(placeholder):
-    st.chat_message('user').markdown(prompt)
-    add_to_history('user', prompt)
+instr = 'Hi there! Enter what you want to let me know here.'
+with st.form('chat_input_form'):
+    input_col, btn_col = st.columns([8.5, 1])
 
-    with st.chat_message('assistant'):
-        res_box = st.empty()
-        full_res = ''
-        gpt_res = get_gpt_response(prompt)
-        next(gpt_res)  # Remove the role entry
-        for chunk in gpt_res:
-            if new_text := chunk['choices'][0]['delta']:
-                full_res += new_text['content']
-                res_box.markdown(full_res + ':rainbow:')
-            else:
-                # We hit the end of the GPT response
-                res_box.markdown(full_res)
+    with input_col:
+        prompt = st.text_input(
+            instr,
+            value=init_prompt,
+            placeholder=instr,
+            label_visibility='collapsed'  # Hide the label
+        )
 
-        add_to_history('assistant', full_res)
+    with btn_col:
+        submitted = st.form_submit_button('Chat')
+
+    if prompt and submitted:
+        chat_room.chat_message('user').markdown(prompt)
+        add_to_history(prompt, 'user')
+
+        with chat_room.chat_message('assistant'):
+            msg = st.empty()
+            full_res = ''
+            gpt_res = get_gpt_response(ND_PROMPT + prompt)
+            next(gpt_res)  # Remove the role entry
+            for chunk in gpt_res:
+                if new_text := chunk['choices'][0]['delta']:
+                    full_res += new_text['content']
+                    msg.markdown(full_res + ':rainbow:')
+                else:
+                    # We hit the end of the GPT response
+                    msg.markdown(full_res)
+
+            add_to_history(full_res, 'assistant')
